55d0f1ee6a70c497f17956e9f571137709588771
harsh-9in <harshkumar9.in@gmail.com>
2021-11-01 18:44:29 +0530

    Slide Icons Created

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,7 +4,9 @@ import './Sidebar.css';
 import AddIcon from '@material-ui/icons/Add';
 import SidebarOption from './SidebarOption';
 import InboxIcon from '@material-ui/icons/Inbox';
-
+import StarIcon from '@material-ui/icons/Star';
+// import SentIcon from '@material-ui/icons/Sent';
+// import StarIcon from '@material-ui/icons/Star';
 
 function Sidebar(){
     return(
@@ -14,7 +16,10 @@ function Sidebar(){
             </Button>
 
             <SidebarOption Icon={InboxIcon} title="inbox" number={54}/>
-            
+            <SidebarOption Icon={StarIcon} title="Starred" number={0}/>
+            {/* <SidebarOption Icon={SentIcon} title="inbox" number={54}/>
+            <SidebarOption Icon={InboxIcon} title="inbox" number={54}/>
+             */}
         </div>
     );
 }
