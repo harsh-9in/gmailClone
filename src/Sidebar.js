c067bd866e67edca53c035f12d84a5f1a74beb8d
harsh-9in <harshkumar9.in@gmail.com>
2021-11-01 20:20:34 +0530

    SlidebarFootericons

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,10 @@
-import { Button } from '@material-ui/core';
+import { Button, IconButton } from '@material-ui/core';
 import React from 'react';
 import './Sidebar.css';
 import AddIcon from '@material-ui/icons/Add';
 import SidebarOption from './SidebarOption';
-import { LabelImportant,Inbox,Star,AccessTime,NearMe } from '@material-ui/icons';
+import { LabelImportant,Inbox,Star,AccessTime,NearMe,
+        ExpandMore,Person,Duo, Phone } from '@material-ui/icons';
 
 function Sidebar(){
     return(
@@ -17,8 +18,21 @@ function Sidebar(){
             <SidebarOption Icon={AccessTime} title="Snoozed" number={0}/>
             <SidebarOption Icon={LabelImportant} title="Important" number={54}/>
             <SidebarOption Icon={NearMe} title="Sent" number={0}/>
-            
+            <SidebarOption Icon={ExpandMore} title="More" number={0} />
+
+
+            <div className='sidebarFooter'>
+                <div className='sidebarFooterIcons'>
+                    <IconButton> <Person/> </IconButton>
+                    <IconButton> <Duo/> </IconButton>
+                    <IconButton> <Phone/> </IconButton>
+                    
+                </div>
+            </div>
+
+
         </div>
+
     );
 }
 
